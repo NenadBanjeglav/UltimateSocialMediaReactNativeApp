f5792308371b0a95eaaaca7b7ae7abc8adc07924
Nenad <nenadmobmail@gmail.com>
2025-03-23 16:03:41 +0100

    feedPostItem

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
+import FeedPostItem from "@/components/FeedPostItem";
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import dummyPosts from "@/dummyPosts";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text className="text-2xl text-red-500 font-bold m-10 text-center">
-        Open up App.tsx to start working on your app!
-      </Text>
+      <FlatList
+        data={dummyPosts}
+        renderItem={({ item }) => <FeedPostItem post={item} />}
+      />
 
-      <View className="w-24 h-24 bg-lime-200 border-2 border-pink-500 rounded-full"></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    paddingTop: 80,
   },
 });
