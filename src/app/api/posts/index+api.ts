10df2dfd2fd3ea9809502489b133338e1208d16d
Nenad <nenadmobmail@gmail.com>
2025-04-04 20:58:48 +0200

    neon postgress db

--- src/app/api/posts/index+api.ts
+++ src/app/api/posts/index+api.ts
@@ -1,8 +1,19 @@
 import dummyPosts from "@/dummyPosts";
+import { neon } from "@neondatabase/serverless";
 
-export function GET(request: Request) {
+const sql = neon(
+  "postgresql://neondb_owner:npg_9ONaURlAw0DL@ep-crimson-darkness-a2dj0y9m-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+);
+
+export async function GET(request: Request) {
   //read posts from db
   const posts = dummyPosts;
+
+  //test db connectiong
+
+  const result = await sql`SELECT version()`;
+  console.log(result);
+
   return Response.json({ posts });
 }
 
